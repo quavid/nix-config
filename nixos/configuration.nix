a43f9c20bbf304b51b5c362509cf86bd7ae07e8a
quavid <quentin.david.jones@gmail.com>
2025-09-13 16:26:23 -0500

    configuration.nix updated to reflect previous configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -68,25 +68,94 @@
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
-  # FIXME: Add the rest of your current configuration
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    dmenu
+    xorg.xev
+    xorg.xmodmap
+    xclip
+    vesktop
+  ];
+  programs = {
+    firefox.enable = true;
+    steam.enable = true;
+  };
 
-  # TODO: Set your hostname
-  networking.hostName = "your-hostname";
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+  ];
 
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/nvme0n1";
+    useOSProber = true;
+ };
+
+  services.displayManager = {
+    gdm.enable = true;
+    defaultSession = "none+xmonad";
+  };
+
+  services.xserver = {
+    enable = true;
+    videoDrivers = ["nvidia"];
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      };
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+  hardware.graphics = {
+    enable = true;
+    nvidia = {
+      modesetting.enable = true;
+      nvidiaSettings = true;
+      open = false;
+    };
+  };
+
+  time.timeZone = "America/Chicago";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = { 
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    }; 
+  };
+  
+  services.printing.enable = true;
+  
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  # Setting Hostname
+  networking.hostName = "shibboleth";
+  networking.networkmanager.enable = true;
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
-    # FIXME: Replace with your username
-    your-username = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "correcthorsebatterystaple";
+    davidj = {
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel"];
+      extraGroups = ["wheel" "networkmanager"];
     };
   };
 
@@ -104,5 +173,5 @@
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.05";
+  system.stateVersion = "25.05";
 }
