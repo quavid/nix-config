398e335e044d112cbccb0cd3121ee3ad4341158b
Q David Jones <quentin.david.jones@gmail.com>
2025-09-24 21:35:53 -0500

    added flake inputs for nixcord & nixvim

--- flake.nix
+++ flake.nix
@@ -12,8 +12,13 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follow = "nixpkgs";
+    };
+    nixcord.url = "github:kaylorben/nixcord";
   };
-
   outputs = {
     self,
     nixpkgs,
